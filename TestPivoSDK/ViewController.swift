e9f652346d19c49d2c6b376dddec548f87be0582
Bob Wakefield <bob@cockleburr.net>
2022-07-22 19:42:28 -0500

    Initial Commit

---
+++ TestPivoSDK/ViewController.swift
@@ -0,0 +1,23 @@
+// ViewController.swift
+//
+// Created by Bob Wakefield on 7/22/22.
+// for TestPivoSDK
+//
+// Using Swift 5.0
+// Running on macOS 12.4
+//
+// 
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
